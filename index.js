36ce7c5ec628803e5dfba1e4e583921ef3111d21
Deamer <22369xxxx@qq.com>
2022-12-02 10:00:00 +0800

    bug#52

--- index.js
+++ index.js
@@ -10,27 +10,38 @@ const devtools = {
 	orientation: undefined,
 };
 
-const threshold = 160;
+const threshold = 170;
 
 const emitEvent = (isOpen, orientation) => {
-	globalThis.dispatchEvent(new globalThis.CustomEvent('devtoolschange', {
-		detail: {
-			isOpen,
-			orientation,
-		},
-	}));
+	globalThis.dispatchEvent(
+		new globalThis.CustomEvent("devtoolschange", {
+			detail: {
+				isOpen,
+				orientation,
+			},
+		})
+	);
 };
 
-const main = ({emitEvents = true} = {}) => {
-	const widthThreshold = globalThis.outerWidth - globalThis.innerWidth > threshold;
-	const heightThreshold = globalThis.outerHeight - globalThis.innerHeight > threshold;
-	const orientation = widthThreshold ? 'vertical' : 'horizontal';
+const main = ({ emitEvents = true } = {}) => {
+	const widthThreshold =
+		globalThis.outerWidth - globalThis.innerWidth > threshold;
+	const heightThreshold =
+		globalThis.outerHeight - globalThis.innerHeight > threshold;
+	const orientation = widthThreshold ? "vertical" : "horizontal";
 
 	if (
-		!(heightThreshold && widthThreshold)
-		&& ((globalThis.Firebug && globalThis.Firebug.chrome && globalThis.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)
+		!(heightThreshold && widthThreshold) &&
+		((globalThis.Firebug &&
+			globalThis.Firebug.chrome &&
+			globalThis.Firebug.chrome.isInitialized) ||
+			widthThreshold ||
+			heightThreshold)
 	) {
-		if ((!devtools.isOpen || devtools.orientation !== orientation) && emitEvents) {
+		if (
+			(!devtools.isOpen || devtools.orientation !== orientation) &&
+			emitEvents
+		) {
 			emitEvent(true, orientation);
 		}
 
@@ -46,7 +57,7 @@ const main = ({emitEvents = true} = {}) => {
 	}
 };
 
-main({emitEvents: false});
+main({ emitEvents: false });
 setInterval(main, 500);
 
 export default devtools;
